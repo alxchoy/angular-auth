cd766afb54e053bebceaf93e20b3218b81349caa
Alexander Choy <alexanderchoy18@gmail.com>
2024-09-29 01:11:04 -0500

    add supabase functions

---
+++ supabase/functions/auth-signin/index.ts
@@ -0,0 +1,57 @@
+import { Cookie, setCookie } from "jsr:@std/http/cookie";
+import { createClient } from "jsr:@supabase/supabase-js@2";
+import { corsHeaders } from "../_shared/cors.ts";
+
+Deno.serve(async (req) => {
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
+
+  if (req.method !== "POST") {
+    return new Response("Method Not Allowed", { status: 405 });
+  }
+
+  try {
+    const { email, password } = await req.json();
+    const headers = new Headers({
+      ...corsHeaders,
+      "Content-Type": "application/json",
+    });
+
+    const supabaseClient = createClient(
+      Deno.env.get("PROJECT_URL") ?? "",
+      Deno.env.get("API_KEY") ?? "",
+    );
+    const { data, error } = await supabaseClient.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) throw error;
+
+    const userData = {
+      id: data.user.id,
+      email: data.user.email,
+      metadata: data.user.user_metadata,
+      createdAt: data.user.created_at,
+      updatedAt: data.user.updated_at,
+    };
+
+    const cookie: Cookie = {
+      name: "token",
+      value: data.session.access_token,
+      secure: true,
+      httpOnly: true,
+      sameSite: "Strict",
+      maxAge: data.session.expires_in,
+    };
+
+    setCookie(headers, cookie);
+
+    return new Response(JSON.stringify(userData), { headers });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { "Content-Type": "application/json" },
+      status: 400,
+    });
+  }
+});
